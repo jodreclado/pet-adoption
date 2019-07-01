68ef1533c89d2b7cffc610c9cda49e395866b233
jodreclado <rogersx@uw.edu>
2019-07-01 01:05:52 -0700

    Add async + pet results (ch9)

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
+import Results from "./Results";
 
 const SearchParams = () => {
   const [location, updateLocation] = useState("Seattle, WA");
+  const [pets, setPets] = useState([]);
   const [breeds, updateBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, updateBreed] = useDropdown("Breed", "", breeds);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+  
+    setPets(animals || []);
+  }
 
   useEffect(() => {
     updateBreeds([]);
@@ -19,7 +31,12 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -33,6 +50,7 @@ const SearchParams = () => {
         <BreedDropdown />
         <button>Submit</button>
       </form>
+      <Results pets={pets} />;
     </div>
   );
 };
